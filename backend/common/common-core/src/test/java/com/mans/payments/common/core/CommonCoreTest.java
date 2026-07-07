e1dc343a60ff41fe60d36928773b9c4f6a26b69d
Manjunath <manjunathg0801@gmail.com>
2026-07-07 22:08:50 +0530

    build: configure dependency management and junit testing platform

---
+++ backend/common/common-core/src/test/java/com/mans/payments/common/core/CommonCoreTest.java
@@ -0,0 +1,15 @@
+package com.mans.payments.common.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CommonCoreTest {
+
+    @Test
+    void shouldPass() {
+
+        assertEquals(2, 1 + 1);
+
+    }
+}
